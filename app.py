a2138d1f497216b611886a7f3e92ea32e1c23b66
Mariana González <marianagonzalezlopez01@gmail.com>
2023-10-05 22:55:52 -0600

    integration with angular

--- app.py
+++ app.py
@@ -8,17 +8,18 @@ from bson.objectid import ObjectId
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, ServiceContext, StorageContext, \
     load_index_from_storage
 from langchain import OpenAI
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-uri = ""
-os.environ["OPENAI_API_KEY"] = ''
+uri = "mongodb+srv://movie-mood:bzqk8Yademoviemood@cluster0.acn6syr.mongodb.net/?retryWrites=true&w=majority"
+os.environ["OPENAI_API_KEY"] = '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'
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 
 # ------------------------- DATABASE -------------------------
 
-
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
 # endpoint to check if the database is connected
 @app.route('/api/database-ping', methods=['GET'])
 def get_database_connect():
@@ -31,7 +32,7 @@ def get_database_connect():
 
 # ------------------------- USERS -------------------------
 
-
+#pagina de login digamos
 # endpoint to create new user
 @app.route('/api/users', methods=['POST'])
 def create_user():
@@ -106,6 +107,7 @@ def get_user_by_email(email):
 # ------------------------- MOVIES CATEGORIES -------------------------
 
 # endpoint to add movie category to user
+##categorias al usuario
 @app.route('/api/users/<user_id>/movies-categories', methods=['POST'])
 def add_movie_category(user_id):
     data = request.get_json()
@@ -119,10 +121,13 @@ def add_movie_category(user_id):
 
 
 # ------------------------- ANALYSIS -------------------------
+##endpoint
 # endpoint to get data for user analysis
-@app.route('/api/users/<user_id>/analysis', methods=['GET'])
+@app.route('/api/users/<user_id>/analysis', methods=['POST'])
 def get_user_analysis(user_id):
+    print(request.get_json())
     data = request.get_json()
+    
     if not data or not all(key in data for key in ['age', 'sex', 'state_of_mind', 'film_one', 'film_two',
                                                    'film_three']):
         return jsonify({'error': 'Bad Request', 'message': 'Missing required fields'}), 400
@@ -142,12 +147,13 @@ def get_user_analysis(user_id):
     }
 
     # prompt for chatbot
-    prompt = f""" I want you to act as a movie recommender. I need you to take into account the following user requirements for each of the recommendations: their gender is {''.join(user_data['sex'])}, their age is {''.join(user_data['age'])}, their mood is {''.join(user_data['state_of_mind'])}, their favourite movie genres are {', '.join(user_data['categories'])} and their favourite reference movies are {''.join(user_data['film_one'])} {''.join(user_data['film_two'])} y {''.join(user_data['film_three'])} Recommend movies from the data you were trained with, recommend  movies that are classified according to the user's age. When recommending a movie, please return the title, the year, the genres, the href, the extract and the thumbnail, this information is in the data you were trained with, this, in a json format, and recommend maximum 3 movies. """
+    #prompt = f""" I want you to act as a movie recommender. I need you to take into account the following user requirements for each of the recommendations: their gender is {''.join(user_data['sex'])}, their age is {''.join(user_data['age'])}, their mood is {''.join(user_data['state_of_mind'])}, their favourite movie genres are {', '.join(user_data['categories'])} and their favourite reference movies are {''.join(user_data['film_one'])} {''.join(user_data['film_two'])} y {''.join(user_data['film_three'])} Recommend movies from the data you were trained with, recommend  movies that are classified according to the user's age. When recommending a movie, please return the title, the year, the genres, the href, the extract and the thumbnail, this information is in the data you were trained with, this, in a json format, and recommend maximum 3 movies. """
+    prompt = f""" I want you to act as a movie recommender. I need you to take into account the following user requirements for each of the recommendations: their gender is {user_data['sex']}, their age is {user_data['age']}, their mood is {user_data['state_of_mind']}, their favourite movie genres are {', '.join(user_data['categories'])} and their favourite reference movies are {user_data['film_one']} {user_data['film_two']} y {user_data['film_three']} Recommend movies from the data you were trained with, recommend movies that are classified according to the user's age. When recommending a movie, please return the title, the year, the genres, the href, the extract and the thumbnail, this information is in the data you were trained with, this, in a json format, and recommend 3 movies exactly. """
 
     response_gpt = chatbot(prompt)
     response_gpt = json.loads(response_gpt)
     response_gpt = json.dumps(response_gpt, indent=None, separators=(',', ':'))
-    client.db.users.update_one({'_id': ObjectId(user_id)}, {'$push': {'recommended_films': json.loads(response_gpt)['recommendations']}})
+    client.db.users.update_one({'_id': ObjectId(user_id)}, {'$push': {'recommended_films': json.loads(response_gpt)['movies']}})
     return jsonify({'message': 'Analysis data retrieved successfully', 'response': json.loads(response_gpt)}), 200
 
 
@@ -156,7 +162,7 @@ def train_model():
     index = construct_index("data-training")
     return jsonify({'message': 'Model trained successfully'}), 200
 
-
+##chatbot
 @app.route('/api/chatbot', methods=['POST'])
 def chatbot_response():
     data = request.get_json()
